b431dac2a954ae3dd63dfcc8630655fd375e8424
mammosgeorgios <mammosgeorgios@gmail.com>
2025-08-14 12:50:45 +0000

    fix issues with file permissions

--- run.sh
+++ run.sh
@@ -4,11 +4,13 @@
 sudo apt update
 
 # Common tools/utils
-chmod +x /scripts.tools.sh
+chmod +x ./scripts/tools.sh
 ./scripts/tools.sh
 
 # Any software languages setups
+chmod +x ./scripts/languages.sh
 ./scripts/languages.sh
 
 # Zsh terminal setup 
+chmod +x ./scripts/zsh.sh
 ./scripts/zsh.sh
--- scripts/languages.sh
+++ scripts/languages.sh
@@ -7,4 +7,5 @@ sudo apt install -y openjdk-21-jdk
 sudo apt install -y maven
 
 # Node js
+# TODO: It's probably better to use nvm for nodejs, in order to better specify version
 sudo apt install -y nodejs
